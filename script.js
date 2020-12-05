fe91d03528538857c7530992756395b980327952
franciscocaballero <caballerofrancisco22@yahoo.com>
2020-12-05 18:25:16 -0500

    adding playing state to dice game

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
-
+let playing = true;
 
 // Selecting elements 
 const player0EL = document.querySelector('.player--0');
@@ -23,66 +23,61 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-// Rolling dice functionality 
-let roll = function () {
-    // 1. Generating a random dice roll
-    const dice = Math.trunc(Math.random() * 6) + 1;
-
-    // 2. Display dice 
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`
-
-    // 3. Check for rolled 1: if true
-    if (dice !== 1) {
-        // Add dice to current score
-        currentScore = currentScore + dice;
-        // current0EL.textContent = currentScore; // Change later
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-        // switch to next player
-        document.getElementById(`current--${activePlayer}`).text = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player0EL.classList.toggle('player--active');
-        player1EL.classList.toggle('player--active');
-    }
+const switchPlayer = function () {
+    // switch to next player
+    document.getElementById(`current--${activePlayer}`).text = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0EL.classList.toggle('player--active');
+    player1EL.classList.toggle('player--active');
 }
 
 
+btnRoll.addEventListener('click', function () {
+    if (playing) {
 
-btnRoll.addEventListener('click', roll)
+        // 1. Generating a random dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1;
+
+        // 2. Display dice 
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`
+
+        // 3. Check for rolled 1: if true
+        if (dice !== 1) {
+            // Add dice to current score
+            currentScore = currentScore + dice;
+            // current0EL.textContent = currentScore; // Change later
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            // switch to next player
+            switchPlayer()
+        }
+    }
+})
 
 btnHold.addEventListener('click', function () {
-    // 1. Add current score to active player's score
-    scores[activePlayer] += currentScore;
-    //scores[1] = scores[1] + currentScore
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-    // 2. Check if player's score is >= 100
-    // Finish Game
 
-    // Switch to the next player
+    if (playing) {
 
+        // 1. Add current score to active player's score
+        scores[activePlayer] += currentScore;
+        //scores[1] = scores[1] + currentScore
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        // Switch to the next player
+        // 2. Check if player's score is >= 100
+        if (scores[activePlayer] >= 10) {
+            // Finish Game
+            playing = false;
+            diceEl.classList.add('hidden');
 
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
 
+        } else {
+            switchPlayer()
 
+        }
+    }
 
-    // activePlayer === score0El ? score0El.textContent = totalScore : score1El.textContent = totalScore;
-
-    // if (activePlayer = activePlayer === 0) {
-    //     totalScore += currentScore;
-    //     score0El.textContent = totalScore
-    //     player0EL.classList.toggle('player--active');
-    //     player1EL.classList.add('player--active');
-    //     roll();
-    // } else {
-    //     totalScore += currentScore;
-    //     score1El.textContent = totalScore
-    //     player1EL.classList.a('player--active');
-    // }
-    // if (scores[0] >= 100 || scores[1] >= 100) {
-    //     activePlayer = winner;
-    // }
-
-    // // switch to next player
-    // activePlayer = activePlayer === 0 ? player0EL.classList.add('player--active') :
 })
